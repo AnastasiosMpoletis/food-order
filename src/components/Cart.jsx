f38958a8c70073aba96e714f62f1b6e8084b972e
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-07 18:33:05 +0300

    L-292. Create CartItem component that displays meals in Cart. Apply increase, decrease quantity functionality in Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
     const cartCtx = use(CartContext);
@@ -19,9 +20,14 @@ export default function Cart() {
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map(item =>
-                    <li key={item.id}>
-                        {item.name} - {item.quantity}
-                    </li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                        onIncrease={() => cartCtx.addItem(item)}
+                    />
                 )}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
